e6875a319c1433e1023e5c09e0825f950ba46ddd
nishi kosei <24karamawariken@gmail.com>
2014-07-31 16:32:36 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -482,8 +482,8 @@ $(function() {
 
           var leftDayText = "";
           if (leftDay == 0) {
-            <h1>
-            leftDayText = "今日";</h1>
+           
+            leftDayText ='<h1>'+"今日"+'</h1>';
           } else if (leftDay == 1) {
             <h2>leftDayText = "明日";</h2>
           } else if (leftDay == 2) {
