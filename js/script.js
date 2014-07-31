d8d1b6280cb49f510210c1eaa2bdbe1f373c60ea
nishi kosei <24karamawariken@gmail.com>
2014-07-31 16:12:37 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -569,6 +569,11 @@ $(function() {
   $("#select_area").change(function(data) {
     var row_index = $(data.target).val();
     onChangeSelect(row_index);
+    new PNotify({
+            title: 'Regular Notice',
+            text: 'Check me out! I\'m a notice.'
+        });
+  
   });
 
   //-----------------------------------
